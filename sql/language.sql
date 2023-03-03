834e756c94af27e0953c51396e7e0331cb5ef5d0
FRanggaY <franciscusrangga@gmail.com>
2023-03-03 14:29:41 +0700

    update language schema

--- sql/language.sql
+++ sql/language.sql
@@ -5,11 +5,10 @@ DROP TABLE IF EXISTS languages
 CREATE TABLE languages (
 	id TEXT(5) NOT NULL, 
     title TEXT(10) NOT NULL,
+    "name" TEXT(10) NOT NULL,
+    proficieny TEXT(10) NOT NULL,
+    "image" TEXT(65),
 	createdAt TEXT(10) NOT NULL,
 	updatedAt TEXT(10) NOT NULL,
     isActive TEXT(1) DEFAULT (0) NOT NULL
 );
-
-INSERT INTO languages (id, title, createdAt, updatedAt, isActive) VALUES ('l0001', 'in_ID', '3/2/2023', '3/2/2023', '1');
-
-INSERT INTO languages (id, title, createdAt, updatedAt, isActive) VALUES ('l0002', 'en_US', '3/2/2023', '3/2/2023', '1');
