3bf1a60a39befcb2233432422884280b5e58fec8
FRanggaY <franciscusrangga@gmail.com>
2023-04-23 16:24:07 +0700

    disable seo for test prod

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -11,56 +11,56 @@ import { getData } from '@/utils/getData'
 // json
 import projectPlatforms from '@/data/locales/en_US/json/projectPlatforms.json'
 
-const seo_title = 'Home | ' + process.env.YOURNAME;
-const seo_description = 'This is home page';
-const seo_icon = '/assets/image/my-logo.png'
+// const seo_title = 'Home | ' + process.env.YOURNAME;
+// const seo_description = 'This is home page';
+// const seo_icon = '/assets/image/my-logo.png'
 
-export const metadata: Metadata = {
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  },
-  title: seo_title,
-  description: seo_description,
-  icons: {
-    icon: seo_icon,
-    shortcut: seo_icon,
-    apple: seo_icon,
-    other: {
-      rel: 'apple-touch-icon-precomposed',
-      url: seo_icon,
-    },
-  },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  openGraph: {
-    title: seo_title,
-    description: seo_description,
-    url:  process.env.SEO_URL,
-    siteName: process.env.SEO_SITE_NAME,
-    locale: process.env.SEO_LOCALE,
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: seo_title,
-    description: seo_description,
-    site: '@' + process.env.SEO_CREATOR,
-    creator: '@' + process.env.SEO_CREATOR
-  },
-};
+// export const metadata: Metadata = {
+//   viewport: {
+//     width: 'device-width',
+//     initialScale: 1,
+//     maximumScale: 1,
+//   },
+//   title: seo_title,
+//   description: seo_description,
+//   icons: {
+//     icon: seo_icon,
+//     shortcut: seo_icon,
+//     apple: seo_icon,
+//     other: {
+//       rel: 'apple-touch-icon-precomposed',
+//       url: seo_icon,
+//     },
+//   },
+//   robots: {
+//     index: false,
+//     follow: true,
+//     nocache: true,
+//     googleBot: {
+//       index: true,
+//       follow: false,
+//       noimageindex: true,
+//       'max-video-preview': -1,
+//       'max-image-preview': 'large',
+//       'max-snippet': -1,
+//     },
+//   },
+//   openGraph: {
+//     title: seo_title,
+//     description: seo_description,
+//     url:  process.env.SEO_URL,
+//     siteName: process.env.SEO_SITE_NAME,
+//     locale: process.env.SEO_LOCALE,
+//     type: 'website',
+//   },
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: seo_title,
+//     description: seo_description,
+//     site: '@' + process.env.SEO_CREATOR,
+//     creator: '@' + process.env.SEO_CREATOR
+//   },
+// };
 
 export default async function Page({ params: { lang } }: any) {
   // get title from data
