fdcf571f52990f4f613f5b7c23c49277a9f0527f
FRanggaY <franciscusrangga@gmail.com>
2023-04-23 16:05:09 +0700

    test three seo case

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Metadata } from 'next';
+import { Metadata } from 'next';
 // to handle language selection
 import { packValueChecker } from '@/utils/functionlangChecker'
 import { locales } from '@/data/dataTranslation'
@@ -8,55 +8,55 @@ import SectionAbout from './sectionAbout'
 // utils
 import { getData } from '@/utils/getData'
 
-// const seo_title = 'About | ' + process.env.YOURNAME;
-// const seo_description = 'This is about page';
-// const seo_icon = '/assets/image/my-logo.png'
+const seo_title = 'About | ' + process.env.YOURNAME;
+const seo_description = 'This is about page';
+const seo_icon = '/assets/image/my-logo.png'
 
-// export const metadata: Metadata = {
-//   viewport: {
-//     width: 'device-width',
-//     initialScale: 1,
-//     maximumScale: 1,
-//   },
-//   title: seo_title,
-//   icons: {
-//     icon: seo_icon,
-//     shortcut: seo_icon,
-//     apple: seo_icon,
-//     other: {
-//       rel: 'apple-touch-icon-precomposed',
-//       url: seo_icon,
-//     },
-//   },
-//   robots: {
-//     index: false,
-//     follow: true,
-//     nocache: true,
-//     googleBot: {
-//       index: true,
-//       follow: false,
-//       noimageindex: true,
-//       'max-video-preview': -1,
-//       'max-image-preview': 'large',
-//       'max-snippet': -1,
-//     },
-//   },
-//   openGraph: {
-//     title: seo_title,
-//     description: seo_description,
-//     url:  process.env.SEO_URL + "/about",
-//     siteName: seo_title,
-//     locale: process.env.SEO_LOCALE,
-//     type: 'website',
-//   },
-//   twitter: {
-//     card: 'summary_large_image',
-//     title: seo_title,
-//     description: seo_description,
-//     site: '@' + process.env.SEO_CREATOR,
-//     creator: '@' + process.env.SEO_CREATOR
-//   },
-// };
+export const metadata: Metadata = {
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  title: seo_title,
+  icons: {
+    icon: seo_icon,
+    shortcut: seo_icon,
+    apple: seo_icon,
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: seo_icon,
+    },
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: seo_title,
+    description: seo_description,
+    url:  process.env.SEO_URL + "/about",
+    siteName: seo_title,
+    locale: process.env.SEO_LOCALE,
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: seo_title,
+    description: seo_description,
+    site: '@' + process.env.SEO_CREATOR,
+    creator: '@' + process.env.SEO_CREATOR
+  },
+};
 
 export default async function Page({ params: { lang } }: any) {
   const langValue = packValueChecker(lang)
