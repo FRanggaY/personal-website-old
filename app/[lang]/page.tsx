458574a3c9abd51e1638c5ffc1d275cd94965141
FRanggaY <franciscusrangga@gmail.com>
2023-11-21 08:54:00 +0700

    fixing : update next, add sitemap, fixing seo and minor warn

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Metadata } from 'next';
 // to handle language selection
 import { packValueChecker } from '@/utils/functionlangChecker'
 import { locales } from '@/data/dataTranslation'
@@ -10,46 +9,43 @@ import SectionWorks from './sectionWorks'
 import { getData } from '@/utils/getData'
 // json
 import projectPlatforms from '@/data/locales/en_US/json/projectPlatforms.json'
+import siteMetadata from '@/utils/siteMetaData';
 
-const seo_title = 'Home | ' + process.env.YOURNAME;
-const seo_description = 'This is home page';
-const seo_icon = '/assets/image/my-logo.png'
+const seo_title = 'Home | ' + siteMetadata.title;
+const seo_url =  process.env.SEO_URL;
 
-export const metadata: Metadata = {
-  title: seo_title,
-  description: seo_description,
-  icons: {
-    icon: seo_icon,
-    shortcut: seo_icon,
-    apple: seo_icon,
-    other: {
-      rel: 'apple-touch-icon-precomposed',
-      url: seo_icon,
-    },
+export const metadata = {
+  metadataBase: new URL(seo_url ?? ''),
+  title: {
+    template: `%s | ${siteMetadata.title}`,
+    default: seo_title, // a default is required when creating a template
   },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-    },
-  },
+  description: siteMetadata.description,
   openGraph: {
     title: seo_title,
-    description: seo_description,
-    url:  process.env.SEO_URL,
-    type: 'website',
-    locale: process.env.SEO_LOCALE,
-    siteName: process.env.SEO_SITE_NAME,
+    description: siteMetadata.description,
+    url:  seo_url,
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: siteMetadata.locale,
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title: seo_title,
-    description: seo_description,
-    site: '@' + process.env.SEO_CREATOR,
-    creator: '@' + process.env.SEO_CREATOR
+    images: [siteMetadata.socialBanner],
   },
 };
 
--- app/[lang]/service/page.tsx
+++ app/[lang]/service/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Metadata } from 'next';
 // to handle language selection
 import { packValueChecker } from '@/utils/functionlangChecker'
 import { locales } from '@/data/dataTranslation'
@@ -7,46 +6,43 @@ import { locales } from '@/data/dataTranslation'
 import SectionServices from './sectionServices';
 // utils
 import { getData } from '@/utils/getData'
+import siteMetadata from '@/utils/siteMetaData';
 
-const seo_title = 'Service | ' + process.env.YOURNAME;
-const seo_description = 'This is service page';
-const seo_icon = '/assets/image/my-logo.png'
+const seo_title = 'Service | ' + siteMetadata.title;
+const seo_url =  process.env.SEO_URL + "/service";
 
-export const metadata: Metadata = {
-  title: seo_title,
-  description: seo_description,
-  icons: {
-    icon: seo_icon,
-    shortcut: seo_icon,
-    apple: seo_icon,
-    other: {
-      rel: 'apple-touch-icon-precomposed',
-      url: seo_icon,
-    },
+export const metadata = {
+  metadataBase: new URL(seo_url ?? ''),
+  title: {
+    template: `%s | ${siteMetadata.title}`,
+    default: seo_title, // a default is required when creating a template
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: seo_title,
+    description: siteMetadata.description,
+    url:  seo_url,
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: siteMetadata.locale,
+    type: "website",
   },
   robots: {
     index: true,
     follow: true,
-    nocache: true,
     googleBot: {
       index: true,
       follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
-  openGraph: {
-    title: seo_title,
-    description: seo_description,
-    url:  process.env.SEO_URL + "/service",
-    siteName: seo_title,
-    locale: process.env.SEO_LOCALE,
-    type: 'website',
-  },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title: seo_title,
-    description: seo_description,
-    site: '@' + process.env.SEO_CREATOR,
-    creator: '@' + process.env.SEO_CREATOR
+    images: [siteMetadata.socialBanner],
   },
 };
 
