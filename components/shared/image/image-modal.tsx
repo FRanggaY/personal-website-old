95215d3dec31fcb351373dd61e910c0854549b80
FRanggaY <franciscusrangga@gmail.com>
2024-05-18 14:53:10 +0700

    update : add sharp and fix image

--- components/shared/image/image-modal.tsx
+++ components/shared/image/image-modal.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -12,7 +13,15 @@ const ImageModal: React.FC<ImageModalProps> = ({ src, alt, onClose }) => {
     <Link href="" className='cursor-default' onClick={onClose}>
       <div className='fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50' style={{ backgroundColor: 'rgba(0,0,0,0.7)' }}>
         <div className='relative bg-white rounded-sm'>
-          <img className='max-w-[90vw] max-h-[90vh]' src={src} alt={`${alt} modal`} />
+          <Image
+            className='max-w-[90vw] max-h-[90vh]'
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            width={500}
+            height={300} src={src} alt={`${alt} modal`} />
         </div>
       </div>
     </Link>
