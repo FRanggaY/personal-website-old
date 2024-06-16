4e28932956426dc05974c9d782c6baed506ceb2b
FRanggaY <franciscusrangga@gmail.com>
2024-06-16 22:34:56 +0700

    update : content of project

--- data/static/en/public-profile-project.ts
+++ data/static/en/public-profile-project.ts
@@ -5,57 +5,77 @@ export const data = {
     {
       "id": "2a6357a8-83b3-448b-920d-fc17d395faa6",
       "title": "Motorcycle Storage Facility",
-      "description": null,
+      "description": "Web App Management Motorcycle Storage Facility",
       "slug": "motorcycle-storage-facility",
       "created_at": "2024-05-11 11:20:57",
-      "updated_at": "2024-05-11 11:20:57",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:27:29",
+      "image_url": "",
+      "logo_url": null
+    },
+    {
+      "id": "0f678df0-cd16-48f4-aaa4-9f30aa29b44c",
+      "title": "GatSource Guest Management",
+      "description": "Web App Management Digital Guest Book",
+      "slug": "gatsource-guest-management",
+      "created_at": "2024-06-16 15:27:07",
+      "updated_at": "2024-06-16 15:27:07",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "66768010-dd6a-49d4-9cb6-3d94abc77b72",
       "title": "Personal Portfolio API",
-      "description": null,
+      "description": "API Personal Portfolio",
       "slug": "personal-portfolio-api",
       "created_at": "2024-05-11 11:21:08",
-      "updated_at": "2024-05-11 11:21:08",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:27:51",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "96d40303-c5ca-4523-b0c1-67fec9810f5a",
       "title": "Motorcycle Storage Facility API",
-      "description": null,
+      "description": "API Motorcycle Storage Facility",
       "slug": "motorcycle-storage-facility-api",
       "created_at": "2024-05-11 11:21:18",
-      "updated_at": "2024-05-11 11:21:18",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:28:07",
+      "image_url": "",
+      "logo_url": null
+    },
+    {
+      "id": "cf9f0051-34b3-448c-871e-ff3b1c44d5fc",
+      "title": "GatSource Guest API",
+      "description": "API Digital Guest Book",
+      "slug": "gatsource-guest-api",
+      "created_at": "2024-06-16 15:26:38",
+      "updated_at": "2024-06-16 15:26:38",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "3fd8368e-6468-4fe6-a3a1-70a56dc24289",
       "title": "Personal Portfolio Management",
-      "description": null,
+      "description": "Web App Management Personal Portfolio",
       "slug": "personal-portfolio-management",
       "created_at": "2024-05-11 11:21:32",
-      "updated_at": "2024-05-11 11:21:32",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:28:30",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "62b8180e-693c-4f20-90ea-a6c023c3aaa3",
       "title": "Personal Portfolio",
-      "description": null,
+      "description": "Website Personal Portfolio",
       "slug": "personal-portfolio",
       "created_at": "2024-05-11 10:56:45",
-      "updated_at": "2024-05-11 10:56:45",
-      "image_url": null,
-      "logo_url": null
+      "updated_at": "2024-06-16 15:28:46",
+      "image_url": "",
+      "logo_url": ""
     }
   ],
   "meta": {
     "size": 10,
-    "total": 5,
+    "total": 7,
     "total_pages": 1,
     "offset": 1
   }
--- data/static/id/public-profile-project.ts
+++ data/static/id/public-profile-project.ts
@@ -5,57 +5,77 @@ export const data = {
     {
       "id": "62197df7-f75e-44f0-b22c-349e9ad9bd90",
       "title": "Penitipan Motor",
-      "description": null,
+      "description": "Sistem Manajemen Penitipan Motor",
       "slug": "motorcycle-storage-facility",
       "created_at": "2024-05-11 11:16:48",
-      "updated_at": "2024-05-11 11:16:48",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:29:18",
+      "image_url": "",
+      "logo_url": null
+    },
+    {
+      "id": "81b9cebe-cd11-4ac6-beb8-236d5f596b06",
+      "title": "GatSource Guest",
+      "description": "Sistem Manajemen Buku Tamu Digital",
+      "slug": "gatsource-guest-management",
+      "created_at": "2024-06-16 15:30:09",
+      "updated_at": "2024-06-16 15:30:09",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "cc4fd427-ea49-45b2-9bd6-adb49a982a7a",
       "title": "Portfolio Pribadi API",
-      "description": null,
+      "description": "API Manajemen Portfolio Pribadi",
       "slug": "personal-portfolio-api",
       "created_at": "2024-05-11 11:17:01",
-      "updated_at": "2024-05-11 11:17:01",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:30:40",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "50faee92-5e13-464f-b2cc-7ce3d6a51fcf",
       "title": "Penitipan Motor API",
-      "description": null,
+      "description": "API Manajemen Penitipan Motor",
       "slug": "motorcycle-storage-facility-api",
       "created_at": "2024-05-11 11:18:00",
-      "updated_at": "2024-05-11 11:18:00",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:30:51",
+      "image_url": "",
+      "logo_url": null
+    },
+    {
+      "id": "6274d724-64ad-49e7-9063-db09620fffe6",
+      "title": "GatSource Guest API",
+      "description": "API Manajemen Buku Tamu Digital",
+      "slug": "gatsource-guest-api",
+      "created_at": "2024-06-16 15:31:10",
+      "updated_at": "2024-06-16 15:31:10",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "b0e0f833-4cb2-40a9-a279-b00c9feb31b7",
       "title": "Manajemen Portfolio Pribadi",
-      "description": null,
+      "description": "Sistem Manajemen Portfolio Pribadi",
       "slug": "personal-portfolio-management",
       "created_at": "2024-05-11 11:19:29",
-      "updated_at": "2024-05-11 11:19:29",
-      "image_url": null,
+      "updated_at": "2024-06-16 15:31:31",
+      "image_url": "",
       "logo_url": null
     },
     {
       "id": "514bd6d9-6d17-42a9-8dc9-4f9e93634e82",
       "title": "Portfolio Pribadi",
-      "description": null,
+      "description": "Website Portfolio Pribadi",
       "slug": "personal-portfolio",
       "created_at": "2024-05-11 11:19:45",
-      "updated_at": "2024-05-11 11:19:45",
-      "image_url": null,
-      "logo_url": null
+      "updated_at": "2024-06-16 15:31:45",
+      "image_url": "",
+      "logo_url": ""
     }
   ],
   "meta": {
     "size": 10,
-    "total": 5,
+    "total": 7,
     "total_pages": 1,
     "offset": 1
   }
